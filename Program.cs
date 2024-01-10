b80247e6ec2376b7358260bbd66904d16b92e5ac
Simon Jansen <jansens@asciimation.co.nz>
2024-01-10 16:06:34 +1300

    Initial check in

---
+++ Program.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
+
+namespace GoProWiFiEnabler
+{
+    internal class Program
+    {
+        static DeviceInformation BLEDevice = null;
+        static GattCharacteristic sendCommands = null;
+        static GattCharacteristic APNameCharacteristic = null;
+        static GattCharacteristic APPasswordCharacteristic = null;
+
+        public static string s_readAPName = "b5f90002-aa8d-11e3-9046-0002a5d5c51b";
+        public static string s_readAPPass = "b5f90003-aa8d-11e3-9046-0002a5d5c51b";
+        public static string s_sendCommands = "b5f90072-aa8d-11e3-9046-0002a5d5c51b";
+
+        public static IReadOnlyList<GattDeviceService> servicesList = null;
+
+        static async Task Main(string[] args)
+        {
+            // Specify Bluetooth LE Protocol as per here:
+            // https://learn.microsoft.com/en-us/windows/uwp/devices-sensors/aep-service-class-ids
+
+            string BLESelector = "System.Devices.Aep.ProtocolId:=\"{bb7bb05e-5972-42b5-94fc-76eaa7084d49}\"";
+            DeviceInformationKind deviceInformationKind = DeviceInformationKind.AssociationEndpoint;
+            string[] requiredProperties = { "System.Devices.Aep.Bluetooth.Le.IsConnectable", "System.Devices.Aep.IsConnected" };
+            DeviceWatcher deviceWatcher = DeviceInformation.CreateWatcher(BLESelector, requiredProperties, deviceInformationKind);
+
+            // Register event handlers before starting the watcher.
+            // Added, Updated and Removed are required to get all nearby devices.
+            deviceWatcher.Added += DeviceWatcher_Added;
+            deviceWatcher.Updated += DeviceWatcher_Updated;
+            deviceWatcher.Removed += DeviceWatcher_Removed;
+            // EnumerationCompleted and Stopped are optional to implement.
+            deviceWatcher.EnumerationCompleted += DeviceWatcher_EnumerationCompleted;
+            deviceWatcher.Stopped += DeviceWatcher_Stopped;
+
+            Console.WriteLine("Watching for Bluetooth devices...");
+            // Start the watcher.
+            deviceWatcher.Start();
+
+            while (true)
+            {
+                Boolean bWiFiAPOn = false;
+
+                // If we haven't seen our device yet, keep waiting.
+                if (BLEDevice == null)
+                {
+                    Thread.Sleep(200);
+                }
+                else
+                {
+                    Console.WriteLine("Pairing with GoPro...");
+                    BluetoothLEDevice bluetoothLeDevice = await BluetoothLEDevice.FromIdAsync(BLEDevice.Id);
+                    bluetoothLeDevice.DeviceInformation.Pairing.Custom.PairingRequested += Custom_PairingRequested;
+
+                    if (bluetoothLeDevice.DeviceInformation.Pairing.CanPair)
+                    {
+                        DevicePairingProtectionLevel dppl = bluetoothLeDevice.DeviceInformation.Pairing.ProtectionLevel;
+                        DevicePairingResult dpr = await bluetoothLeDevice.DeviceInformation.Pairing.Custom.PairAsync(DevicePairingKinds.ConfirmOnly, dppl);
+
+                        if (dpr.Status != DevicePairingResultStatus.Paired)
+                        {
+                            Console.WriteLine("Pairing result: " + dpr.Status.ToString());
+                            Console.WriteLine("Press any key to exit...");
+                            Console.ReadKey();
+                            break;
+                        }
+                        Console.WriteLine("Successfully paired.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Device can not be paired.");
+                        if (bluetoothLeDevice.DeviceInformation.Pairing.IsPaired)
+                        {
+                            Console.WriteLine("Device is already paired.");
+                        }                        
+                    }
+
+                    // Get all of the services.
+                    Console.WriteLine("Getting services...");
+                    GattDeviceServicesResult getServicesResult = await bluetoothLeDevice.GetGattServicesAsync();
+
+                    if (getServicesResult.Status == GattCommunicationStatus.Success)
+                    {
+                        // As we get each service store them.
+                        servicesList = getServicesResult.Services;                                              
+                    }
+
+                    foreach (var service in servicesList)
+                    {
+                        Console.WriteLine("Service UUID: " + service.Uuid);
+                        GattCharacteristicsResult characteristicsResult = await service.GetCharacteristicsAsync();
+
+                        if (characteristicsResult.Status == GattCommunicationStatus.Success)
+                        {
+                            var characteristics = characteristicsResult.Characteristics;
+                            foreach (var characteristic in characteristics)
+                            {
+                                Console.WriteLine("Found characteristic: " + characteristic.Uuid.ToString());
+
+                                // Store the characteristics we use later.
+                                // For sending commands (to enable WiFi AP).
+                                if (characteristic.Uuid.ToString() == s_sendCommands)
+                                {
+                                    sendCommands = characteristic;
+                                }
+                                // AP name.
+                                if (characteristic.Uuid.ToString() == s_readAPName)
+                                {
+                                    APNameCharacteristic = characteristic;
+                                }
+                                // Ap password.
+                                if (characteristic.Uuid.ToString() == s_readAPPass)
+                                {
+                                    APPasswordCharacteristic = characteristic;
+                                }
+
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Failed to get characteristics: " + characteristicsResult.Status.ToString());
+                        }
+                    }
+
+                    // Get the AP name by reading the characteristics properties.
+                    if (APNameCharacteristic != null)
+                    {
+                        GattCharacteristicProperties properties = APNameCharacteristic.CharacteristicProperties;
+                        if (properties.HasFlag(GattCharacteristicProperties.Read))
+                        {
+                            // This characteristic supports reading from it.
+                            GattReadResult readResult = await APNameCharacteristic.ReadValueAsync();
+                            if (readResult.Status == GattCommunicationStatus.Success)
+                            {
+                                DataReader dataReader = Windows.Storage.Streams.DataReader.FromBuffer(readResult.Value);
+                                string data = dataReader.ReadString(readResult.Value.Length);
+                                Console.WriteLine("AP Name: " + data);
+                            }
+                        }
+                    }
+
+                    // Get the AP password by reading the characteristics properties.
+                    if (APPasswordCharacteristic != null)
+                    {
+                        GattCharacteristicProperties properties = APPasswordCharacteristic.CharacteristicProperties;
+                        if (properties.HasFlag(GattCharacteristicProperties.Read))
+                        {
+                            // This characteristic supports reading from it.
+                            GattReadResult readResult = await APPasswordCharacteristic.ReadValueAsync();
+                            if (readResult.Status == GattCommunicationStatus.Success)
+                            {
+                                DataReader dataReader = Windows.Storage.Streams.DataReader.FromBuffer(readResult.Value);
+                                string data = dataReader.ReadString(readResult.Value.Length);
+                                Console.WriteLine("AP Password: " + data);
+                            }
+                        }
+                    }
+
+                    // Turn on WiFi.
+                    GattCommunicationStatus sendCommandResult = GattCommunicationStatus.Unreachable;
+                    if (sendCommands != null && !bWiFiAPOn)
+                    {
+                        Console.WriteLine("Turning on WiFi access point...");
+                        DataWriter mm = new DataWriter();
+                        mm.WriteBytes(new byte[] { 0x03, 0x17, 0x01, 0x01 });
+                        sendCommandResult = await sendCommands.WriteValueAsync(mm.DetachBuffer());
+
+                        if (sendCommandResult == GattCommunicationStatus.Success)
+                        {
+                            Console.WriteLine("WiFi AP successfully enabled.");
+                            bWiFiAPOn = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Failed to send command: " + sendCommandResult.ToString());
+                        }
+                    }                                        
+                    else
+                    {               
+                         Console.WriteLine("Failed to get services: " + getServicesResult.Status.ToString());
+                    }
+
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey();
+                    break;
+                }
+            }
+        }
+
+        private static void DeviceWatcher_Stopped(DeviceWatcher sender, object args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void DeviceWatcher_EnumerationCompleted(DeviceWatcher sender, object args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void DeviceWatcher_Removed(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            // We only care about the GoPro device.
+        }
+
+        private static void DeviceWatcher_Updated(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            // We only care about the GoPro device.
+        }
+
+        private static void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation args)
+        {
+            if (!string.IsNullOrWhiteSpace(args.Name))
+            {
+                Console.WriteLine(args.Name);
+                // If the device is a GoPro we set the device to it.                
+                if (args.Name.Contains("GoPro"))
+                {
+                    BLEDevice = args;
+                }
+            }
+        }
+
+        private static void Custom_PairingRequested(DeviceInformationCustomPairing sender, DevicePairingRequestedEventArgs args)
+        {
+            // On initial pairing we much accept the request.
+            Console.WriteLine("Confirming pairing request...");
+            args.Accept();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,81 @@
+
 # GoPro_WiFi_Enabler
-A minimal C# Windows Console app that connects to a GoPro camera and used Bluetooth to enable the WiFi access point.
+
+A minimal C# Windows Console app that connects to a GoPro camera via Bluetooth to enable the WiFi access point so you can connect to it from another computer witout needing a USB cable or using a SD Card reader.
+
+## Description
+
+GoPro cameras have inbuilt WiFi but in order to turn on the access point so other WiFi devices can connect to it you must enable it first via a Bluetooth LE command.
+
+This app when run on a, Windows 11 machine, will look for Bluetooth devices and connect to the first GoPro camera it finds.
+
+It will then attempt to pair with the device via Bluetooth if not already paired.
+
+Once paired it will list the Services and Characteristics available (described in the link below for the OpenGoPro BLE 2.0 Specs).
+
+It will print to the console the Username and Password to use to connect to the WiFi access point then it will enable the access point so you can connect to it using them.
+
+This was built so I could avoid using the Quik phone app to connect to a GoPro camera in order to enable the WiFi access point on the camera via a Windows 11 machine.
+
+I want to be able to connect wirelesly and transfer files from the GoPro cameras internal server straight to my PC using WinSCP. 
+
+This is the most minimal code needed, have no real error handling and has only been tested minimally with a GoPro Hero 12 Black and Windows 11.
+
+## Getting Started
+
+### Dependencies
+
+I had to add the Microsoft.Windows.SDK.Contracts Nuget package as per the link given below.
+
+### Installing
+
+Download the code and build in Visual Studio.
+
+### Executing program
+
+Either run via the debugger or build and run the built GoProWiFiEnabler.exe file.
+
+Once it has paired and enabled the access point you can connect to the WiFi using the username and password.
+
+In Windows you must connect to the locked 'Unknown' WiFi network then enter the name as the SSID.
+
+Once connected to the GoPro's WiFi, the media server is at: http://10.5.5.9/videos/DCIM/100GOPRO/
+
+In WinSCP you can connect to it using the WebDAV protocol and with that URL as the HostName on Port 80.
+No username or password is required.
+
+## Help
+
+When run, this app will wake up the GoPro camera even when it is powered off.
+
+Bluetooth pairing seems a little flakey.
+
+Once the application run and pairs it should remain a paired device in Windows.
+
+If you have issues pairing/connecting you may need to remove the device from the Windows Bluetooth devices list (under Settings|Bluetooth & devices).
+
+Failing that, try turning it off and on again!
+
+## Authors
+
+Simon Jansen
+asciimation@gmail.com
+
+[github.com/Asciimation]https://github.com/Asciimation
+
+## Version History
+
+1.0.0.0 Initial version
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
+
+## Acknowledgments
+
+* [Transfer Files from your GoPro to a PC or Mac using Wi-Fi](https://www.youtube.com/watch?v=5iLR-acPPvA)
+* [Quick C# application to connect with a Bluetooth LE device](https://www.youtube.com/watch?v=CozmqN_iwNs)
+* [OpenGoPro BLE 2.0 Specs](https://gopro.github.io/OpenGoPro/ble_2_0)
+* [OpenGoPro C# Demo code repository](https://github.com/gopro/OpenGoPro/tree/main/demos/csharp/GoProCSharpSample)
+* [Microsoft.Windows.SDK.Contracts](https://github.com/dotnet/windows-desktop/blob/main/docs/win10apis/README.md)
+* [Microsoft GATT Client documentation](https://learn.microsoft.com/en-us/windows/uwp/devices-sensors/gatt-client)
